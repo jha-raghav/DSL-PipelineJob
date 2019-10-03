db8fcb2a2567b78ed41042b266654f9ecd854a72
jha-raghav <49937195+jha-raghav@users.noreply.github.com>
2019-10-03 18:08:37 +0530

    Update jobdsl.groovy

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -1,1 +1,10 @@
-
+pipelineJob('DSL-Pipeline-HappyTrip') {
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/jha-raghav/HappyTrip.git')
+            }
+          scriptPath('Code/Jenkins.txt')
+        }
+    }
+}
